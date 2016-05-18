583a42da4889b0c3c708f509f5e6fab22e2a17ba
MajorMaad <quentin.audinot@gmail.com>
2016-05-18 11:40:02 -0400

    Finish Karatsuba + test

--- src/karatsuba/Karatsuba.java
+++ src/karatsuba/Karatsuba.java
@@ -2,23 +2,32 @@ package karatsuba;
 
 import java.math.BigInteger;
 
+/**
+ * Classe contenant l'algorithme de Karatsuba permettant de multiplier
+ * deux BigInteger
+ * @author Quentin Audinot
+ *
+ */
 public class Karatsuba {
 	
 	public static BigInteger karatsuba(BigInteger x, BigInteger y) {
-		int n = Math.max(x.bitLength(), y.bitLength());
-		if (n <= 100) return x.multiply(y);
-		n = (n/2) + (n%2);
+		int k = Math.max(x.bitLength(), y.bitLength());
+		if (k <= 2000) return x.multiply(y);
+		k = (k/2) + (k%2);
 		
-		BigInteger b = x.shiftRight(n);
-		BigInteger a = x.subtract(b.shiftLeft(n));
-		BigInteger d = y.shiftRight(n);
-		BigInteger c = y.subtract(d.shiftLeft(n));
+		// x = a*2^k + b
+		BigInteger a = x.shiftRight(k);
+		BigInteger b= x.subtract(a.shiftLeft(k));
+		// y = c*2^k + d
+		BigInteger c = y.shiftRight(k);
+		BigInteger d = y.subtract(c.shiftLeft(k));
 		
 		BigInteger ac = karatsuba(a, c);
 		BigInteger bd = karatsuba(b, d);
-		BigInteger abcd = karatsuba(a.add(b), c.add(d));
+		BigInteger abcd = karatsuba(a.subtract(b), c.subtract(d)); // (a-b)(c-d)
 		
-		return ac.add(abcd.subtract(ac).subtract(bd).shiftLeft(n)).add(bd.shiftLeft(2*n));
+		// 	   ac * 2^2k         + (ac + bd - (a-b)(c-d)) * 2^k              + bd        
+		return ac.shiftLeft(2*k).add(ac.add(bd.subtract(abcd)).shiftLeft(k)).add(bd);
 	}
 
 }
--- src/karatsuba/TestKaratsuba.java
+++ src/karatsuba/TestKaratsuba.java
@@ -1,12 +1,27 @@
 package karatsuba;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 public class TestKaratsuba {
 	
 	public static void main(String[] args) {
-		BigInteger a = new BigInteger("505000");
-		BigInteger b = new BigInteger("505000");		
+		long start, stop;
+		Random r = new Random();
+		BigInteger a = new BigInteger(1000000, r);
+		BigInteger b = new BigInteger(1000000, r);
+		
+		System.out.println("Nombre de chiffres de a et b : 301 030");
+		
+        start = System.currentTimeMillis(); 
+        BigInteger c = Karatsuba.karatsuba(a, b);
+        stop = System.currentTimeMillis();
+        System.out.println("Temps Karatsuba : " + (stop - start) + "ms");
+
+        start = System.currentTimeMillis(); 
+        BigInteger d = a.multiply(b);
+        stop = System.currentTimeMillis();
+        System.out.println("Temps multiplication classique : " + (stop - start) + "ms");
 	}
 	
 }
